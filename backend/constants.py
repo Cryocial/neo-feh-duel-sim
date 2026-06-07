ca3a3999d42b934f34c6474ca73cc31106827afe
Cryocial <cryocial@gmail.com>
2026-06-07 22:19:54 +0530

    refactor: refactored backend to be a bit more practical

---
+++ backend/constants.py
@@ -0,0 +1,31 @@
+from enum import Enum, auto
+
+class MovementType(Enum):
+    INFANTRY = auto()
+    ARMOR = auto()
+    CAVALRY = auto()
+    FLIER = auto()
+
+class WeaponType(Enum):
+    SWORD = auto()
+    LANCE = auto()
+    AXE = auto()
+    BOW = auto()
+    DAGGER = auto()
+    TOME = auto()
+    STAFF = auto()
+    DRAGON = auto()
+    BEAST = auto()
+
+class Color(Enum):
+    RED = auto()
+    BLUE = auto()
+    GREEN = auto()
+    COLORLESS = auto()
+
+class StatType(Enum):
+    HP = "hp"
+    ATK = "atk"
+    SPD = "spd"
+    DEF = "defense"
+    RES = "res"
