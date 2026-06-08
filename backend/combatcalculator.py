7f64ce4c28c8d18e79c3855570959ab8d1cadd9a
Nassim Bouberbachene <nassimbouberbachene@gmail.com>
2026-06-08 16:21:24 +0200

    feat: introduce CombatantInfo dataclass and refactor Strike dataclass

--- backend/combatcalculator.py
+++ backend/combatcalculator.py
@@ -1,16 +1,29 @@
 import math
 from dataclasses import dataclass, field
+from typing import Literal
 from .classes import Unit, StatBlock
-from .constants import WeaponType, Color
+from .constants import WeaponType, Color, StrikeType
 from .jsonbootupstuff import STATUS_EFFECT_DATABASE
+
+UnitRole = Literal["attacker", "defender"]
+
+
+@dataclass
+class CombatantInfo:
+    unit: Unit
+    current_hp: int
+    current_cooldown: int
+    combat_stats: StatBlock | None = None
+    defensive_stat: Literal["def", "res"] | None = None
 
 
 @dataclass
 class Strike:
     """Represents a single attack in the combat sequence."""
-
-    striker: Unit
-    target: Unit
+    striker: UnitRole
+    target: UnitRole
+    strike_type: StrikeType
+    consecutive: bool = False
 
 
 @dataclass
@@ -22,17 +35,27 @@ class CombatEngine:
 
     attacker: Unit
     defender: Unit
-    attacker_combat_stats: StatBlock = field(init=False)  # in combat stats
-    defender_combat_stats: StatBlock = field(init=False)
-    attacker_targeted_stat: int = field(
-        init=False
-    )  # encoding which stat to use when attacker takes a hit: 0 for physical (def), 1 for magical (res)
-    defender_targeted_stat: int = field(init=False)
+    infos: dict[UnitRole, CombatantInfo] = field(init=False)
 
     def simulate(self) -> dict[str, int]:
         """
         Runs the full combat simulation and returns the final HP for both units.
         """
+        self.infos = {
+            "attacker": CombatantInfo(
+                unit=self.attacker,
+                current_hp=self.attacker.base_stats.hp,
+                current_cooldown=self.attacker.current_cooldown,
+            ),
+            "defender": CombatantInfo(
+                unit=self.defender,
+                current_hp=self.defender.base_stats.hp,
+                current_cooldown=self.defender.current_cooldown,
+            ),
+        }
+
+        self._start_of_turn()
+
         self._phase_before_combat()
 
         self._combat_stat_calculations()
@@ -41,54 +64,46 @@ class CombatEngine:
 
         self._phase_start_of_combat()
 
-        while (
-            len(strike_sequence) > 0
-            and self.attacker.base_stats.hp > 0
-            and self.defender.base_stats.hp > 0
-        ):
-            strike = strike_sequence.pop()
-            self._process_strike(
-                strike.striker, strike.target
-            )  # CD pulses and healing calc in there too
-
-        self._phase_before_first_attack()
-
         # tracks dmg mitigated for reflex
         self.attacker.damage_mitigated_bucket = 0
         self.defender.damage_mitigated_bucket = 0
 
-        if self.attacker.base_stats.hp > 0:
-            self._process_strike(self.attacker, self.defender)
-
-        if self.defender.base_stats.hp > 0:
-            self._process_strike(self.defender, self.attacker)
+        while (
+            len(strike_sequence) > 0
+            and self.infos["attacker"].current_hp > 0
+            and self.infos["defender"].current_hp > 0
+        ):
+            strike = strike_sequence.pop()
+            self._process_strike(strike)  # CD pulses and healing calc in there too
 
         self._phase_after_combat()
 
         return {
-            "attacker_final_hp": self.attacker.base_stats.hp,
-            "defender_final_hp": self.defender.base_stats.hp,
+            "attacker_final_hp": self.infos["attacker"].current_hp,
+            "defender_final_hp": self.infos["defender"].current_hp,
         }
 
     def _process_AoE(self, striker: Unit, target: Unit):
         """Applies AoE damage"""
         ...
 
-    def _process_strike(self, striker: Unit, target: Unit):
+    def _process_strike(self, strike: Strike):
         """Calculates and applies damage for a single weapon swing."""
-        striker.current_cooldown -= striker.get_pulse_amount(
-            "before_every_attack", target
-        )
+        striker = self.infos[strike.striker].unit
+        target = self.infos[strike.target].unit
 
-        raw_atk = striker.get_combat_stat("atk", target)
-        is_magic = striker.weapon_type in {
-            WeaponType.TOME,
-            WeaponType.STAFF,
-            WeaponType.DRAGON,
-            WeaponType.BEAST,
-        }
-        defensive_stat = target.get_combat_stat(
-            "res" if is_magic else "defense", striker
+        # self.infos[strike.striker]["current_cooldown"] -= striker.get_pulse_amount(
+        #     "before_every_attack", target
+        # )
+        # self.infos[strike.target]["current_cooldown"] -= striker.get_pulse_amount(
+        #     "before_every_attack", target
+        # )
+
+        raw_atk = self.infos[strike.striker].combat_stats.atk
+        defensive_stat = (
+            self.infos[strike.target].combat_stats.defense
+            if self.infos[strike.target].defensive_stat == "def"
+            else self.infos[strike.target].combat_stats.res
         )
 
         wta = self._get_wta_multiplier(striker, target)
@@ -119,6 +134,7 @@ class CombatEngine:
                 # Add dynamic status true damage
                 if getattr(utilities, "truedmg_logic", None) is not None:
                     true_damage += utilities.truedmg_logic(striker, target)
+
         final_damage = base_damage + true_damage
 
         #  CHECK FOR FIRST HIT DR TYPES
@@ -131,16 +147,14 @@ class CombatEngine:
         # ------------------------------------
 
         target.damage_mitigated_bucket += mitigated_amount
-        new_hp = target.base_stats.hp - final_damage
-        target.base_stats.hp = new_hp
+        self.infos[strike.target].current_hp -= final_damage
 
         charge = (
             1
             + striker.get_pulse_amount("per_unit_attack", target)
             + target.get_pulse_amount("per_foe_attack", striker)
         )
-        striker.current_cooldown -= max(0, charge)
-        striker.combat_attacks_performed += 1
+        self.infos[strike.striker].current_cooldown -= max(0, charge)
 
     def _get_wta_multiplier(self, striker: Unit, target: Unit) -> float:
         """Calculates the final WTA multiplier, including Triangle Adept/Cancel Affinity."""
@@ -189,11 +203,15 @@ class CombatEngine:
             case _:
                 return 0
 
+    def _start_of_turn(self):
+        """"""
+        # update cooldowns
+
     def _phase_before_combat(self):
-        """AOE"""
+        """AoE"""
         if self.attacker.has_AoE:
             self.attacker.current_cooldown -= self.attacker.get_pulse_amount(
-                "before_combat", self.defender
+                "before_AoE", self.defender
             )
             if self.attacker.current_cooldown == 0:
                 self._process_AoE(self.attacker, self.defender)
@@ -205,11 +223,11 @@ class CombatEngine:
             atk_vals[stat] = self.attacker.get_combat_stat(stat, self.defender)
             def_vals[stat] = self.defender.get_combat_stat(stat, self.attacker)
 
-        self.attacker_combat_stats = StatBlock(**atk_vals)
-        self.defender_combat_stats = StatBlock(**def_vals)
+        self.infos["attacker"].combat_stats = StatBlock(**atk_vals)
+        self.infos["defender"].combat_stats = StatBlock(**def_vals)
 
-        self.attacker_targeted_stat = 0 if self.defender.is_physical() else 1
-        self.defender_targeted_stat = 0 if self.attacker.is_physical() else 1
+        self.infos["attacker"].defensive_stat = "def" if self.defender.is_physical() else "res"
+        self.infos["defender"].defensive_stat = "def" if self.attacker.is_physical() else "res"
 
     def _determine_strike_sequence(self) -> list[Strike]:
         """Determines the number and order of strikes."""
@@ -219,23 +237,11 @@ class CombatEngine:
         """Pre-combat damage and healing effects."""
         ...
 
-    def _phase_start_of_combat(self):
-        self.attacker.current_cooldown -= self.attacker.get_pulse_amount(
-            "start_of_combat", self.defender
-        )
-        self.defender.current_cooldown -= self.defender.get_pulse_amount(
-            "start_of_combat", self.attacker
-        )
-
-    def _phase_before_first_attack(self):
-        self.defender.current_cooldown -= self.defender.get_pulse_amount(
-            "before_first_attack", self.attacker
-        )
-
     def _phase_after_combat(self):
-        self.attacker.current_cooldown -= self.attacker.get_pulse_amount(
-            "after_combat", self.defender
-        )
-        self.defender.current_cooldown -= self.defender.get_pulse_amount(
-            "after_combat", self.attacker
-        )
+        # self.attacker.current_cooldown -= self.attacker.get_pulse_amount(
+        #     "after_combat", self.defender
+        # )
+        # self.defender.current_cooldown -= self.defender.get_pulse_amount(
+        #     "after_combat", self.attacker
+        # )
+        ...
