5c1a6a6274ff62677029ecab25aea859f672bed5
Nassim Bouberbachene <nassimbouberbachene@gmail.com>
2026-08-14 17:54:16 +0200

    implement divine vein mechanic

--- backend/combatcalculator.py
+++ backend/combatcalculator.py
@@ -2,7 +2,7 @@ import math
 from dataclasses import dataclass, field, replace
 from typing import Literal
 
-from .build import Unit, StatBlock
+from .build import Unit, StatBlock, DivineVein
 from .constants import Color, StrikeType, EffectType, WeaponType
 from .effects import Effect, build_effect, EFFECT_LIST_MAP
 from .conditions import Phase, Condition, AtomicCondition, AnyOf, AllOf
@@ -30,6 +30,7 @@ class CombatantState:
     spaces_moved: int = 0
     style_enabled: bool = False
     nb_styles: int = 0
+    active_ally_divine_vein: DivineVein | None = None
     granted_visible_buffs: StatBlock = field(default_factory=StatBlock)
     granted_visible_debuffs: StatBlock = field(default_factory=StatBlock)
     effects_start_of_turn: list[Effect] = field(default_factory=list)
@@ -99,6 +100,13 @@ def _distribute_effects(attacker: CombatantState, defender: CombatantState) -> N
             _add_to_bucket(target, effect)
         attacker.nb_styles += status.grants_style
 
+    if attacker.active_ally_divine_vein:
+        for desc in attacker.active_ally_divine_vein.effects:
+            is_self = desc["target"] == "self"
+            effect = build_effect(desc, applied_by="self" if is_self else "foe")
+            target = attacker if is_self else defender
+            _add_to_bucket(target, effect)
+
     defender_skills = filter(
         None,
         [
@@ -126,6 +134,13 @@ def _distribute_effects(attacker: CombatantState, defender: CombatantState) -> N
             target = defender if is_self else attacker
             _add_to_bucket(target, effect)
         defender.nb_styles += status.grants_style
+
+    if defender.active_ally_divine_vein:
+        for desc in defender.active_ally_divine_vein.effects:
+            is_self = desc["target"] == "self"
+            effect = build_effect(desc, applied_by="self" if is_self else "foe")
+            target = defender if is_self else attacker
+            _add_to_bucket(target, effect)
 
 def _add_to_bucket(state: CombatantState, effect: Effect) -> None:
     list_name = EFFECT_LIST_MAP.get(effect.type)
@@ -212,6 +227,8 @@ class CombatEngine:
 
     attacker: Unit
     defender: Unit
+    attacker_divine_vein: DivineVein | None = None
+    defender_divine_vein: DivineVein | None = None
     combatant_states: dict[UnitRole, CombatantState] = field(init=False)
     combat_range: int = field(init=False, default=0)
 
@@ -223,14 +240,16 @@ class CombatEngine:
                 current_hp=self.attacker.current_hp,
                 current_cooldown=self.attacker.max_cooldown - self.attacker.pre_charge,
                 is_initiator=True,
-                style_enabled=self.attacker.style_enabled
+                style_enabled=self.attacker.style_enabled,
+                active_ally_divine_vein=self.attacker_divine_vein
             ),
             "defender": CombatantState(
                 unit=self.defender,
                 current_hp=self.defender.current_hp,
                 current_cooldown=self.defender.max_cooldown - self.defender.pre_charge,
                 is_initiator=False,
-                style_enabled=self.defender.style_enabled
+                style_enabled=self.defender.style_enabled,
+                active_ally_divine_vein=self.defender_divine_vein
             ),
         }
         self._phase_start_of_turn()
